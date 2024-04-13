77913df3db2f8d266eb46dc6338e78bd25815232
yerassylv <bolatkannn@gmail.com>
2024-04-13 13:52:56 +0600

    assig1 deffence

---
+++ src/DefenceAssig1.java
@@ -0,0 +1,33 @@
+public class DefenceAssig1 {
+    public static boolean PalindromeCheck(String test){
+        if(test.isEmpty() || test.length() == 1){
+            return true;
+        }
+        if(test.charAt(0)!= test.charAt(test.length()-1){
+            return false;
+        }
+        return PalindromeCheck()
+
+
+
+
+
+    }
+    public static void main(String[] args){
+        String test = "kazak";
+
+        System.out.println(PalindromeCheck(test));
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
